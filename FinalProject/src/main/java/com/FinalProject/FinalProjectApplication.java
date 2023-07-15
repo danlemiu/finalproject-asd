52fbacee9af8354f013da2aad8b91ca86b260f1b
minhthongtran <minhthong.tran@miu.edu>
2023-07-15 02:15:47 -0500

    part j, k - MinhThongTran - July 15 2023

--- FinalProject/src/main/java/com/FinalProject/FinalProjectApplication.java
+++ FinalProject/src/main/java/com/FinalProject/FinalProjectApplication.java
@@ -1,14 +1,19 @@
 package com.FinalProject;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.custom.aop.FWContext;
 
 @SpringBootApplication
+@ComponentScan("com.FinalProject, com.custom.aop")
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalProjectApplication.class, args);
-		System.out.println("test 3");
+		//SpringApplication.run(FinalProjectApplication.class, args);
+		//System.out.println("test 3");
+		FWContext fwContext = new FWContext();
+		fwContext.start();
 	}
 
 }
