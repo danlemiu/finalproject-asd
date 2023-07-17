e1e96a32312be93318827bf888429bdb870e8b0e
minhthongtran <minhthong.tran@miu.edu>
2023-07-16 22:10:25 -0500

    commit part B

--- FinalProject_PartB/src/main/java/application/IMyTransactionDAO.java
+++ FinalProject_PartB/src/main/java/application/IMyTransactionDAO.java
@@ -4,8 +4,4 @@ import com.FinalProject.Transaction;
 
 public interface IMyTransactionDAO {
     public void save(Transaction transaction);
-
-    public Transaction find(int id);
-
-    public void remove(int id);
 }
